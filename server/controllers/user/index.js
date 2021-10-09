cf9d0ee81ec432007636eed88a79285fbb25245d
js5787 <ljs950430@gmail.com>
2021-10-10 03:45:56 +0900

    feature/server_setting

---
+++ server/controllers/user/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  signup: require('./signup'),
+  signin: require('./signin'),
+  signout: require('./signout'),
+  userinfo: require('./userinfo'),
+  auth: require('./auth'),
+};
---
+++ server/index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const fs = require('fs');
+const https = require('https');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const app = express();
+const {
+  userRouter,
+  ingredientRouter,
+  refrigeratorRouter,
+} = require('./routes');
+
+app.use(express.json());
+app.use(
+  cors({
+    // origin: [process.env.CLIENT],
+    methods: ['GET', 'POST', 'OPTIONS', 'PATCH', 'DELETE'],
+  })
+);
+app.use(cookieParser());
+
+app.use('/user', userRouter);
+app.use('/ingredient', ingredientRouter);
+app.use('/refrigerator', refrigeratorRouter);
+
+const PORT = process.env.HTTP_PORT || process.env.HTTPS_PORT;
+let server;
+if (fs.existsSync('./key.pem') && fs.existsSync('./cert.pem')) {
+  const privateKey = fs.readFileSync(__dirname + '/key.pem', 'utf8');
+  const certificate = fs.readFileSync(__dirname + '/cert.pem', 'utf8');
+  const credentials = { key: privateKey, cert: certificate };
+
+  server = https.createServer(credentials, app);
+  server.listen(PORT, () => console.log(`https server runnning PORT ${PORT}`));
+} else {
+  server = app.listen(PORT, () =>
+    console.log(`http server runnning PORT ${PORT}`)
+  );
+}
+module.exports = server;
