33543b416283b9ddc2011ea0a522e11e76217ec8
seonwoong <lso5936@gmail.com>
2021-10-11 18:03:25 +0900

    Ingredients.js, Refrigerator.css, Refrigerator.js

--- client/src/components/Ingredients.js
+++ client/src/components/Ingredients.js
@@ -1,8 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios'
 import './Ingredients.css'
 
-export default function Ingredients() {
+export default function Ingredients({isAdmin}) {
+
+  const [exContent, setExContent] = useState('')
+  const [nameValue, setNameValue] = useState('')
+  const [methodValue, setMethodValue] = useState('')
 
   const list = ['당근', '양상추', '토마토', '애호박', '양파', '가지', '아스파라거스', '감자', '고구마', '고추', '야채', '이름']
   let listDivision = []
@@ -10,14 +14,14 @@ export default function Ingredients() {
     let realList 
     let explains
 
-  // const getIngredients = () => {
-  //   axios.get('http://localhost:4000/ingredient')
-  //   .then((res) => {
-  //     realList.push(res.목록)
-  //   })
-  //   .catch(err => console.log(err))
-  // }
-  // getIngredients()
+  const getIngredients = () => {
+    // axios.get('http://localhost:4000/ingredient')
+    // .then((res) => {
+    //   realList.push(res.목록)
+    // })
+    // .catch(err => console.log(err))
+  }
+  getIngredients()
 
 
   function makeDivision(list) { //인자에 realList를 넣어줘야 함
@@ -34,10 +38,25 @@ export default function Ingredients() {
   }
   makeDivision(list)
 
+  const addIngredients = () => {
+    axios.post('http://localhost:4000/ingredient', {ingredient_name : `${nameValue}`, keep_method: `${methodValue}`})
+    setNameValue('')
+    setMethodValue('')
+  }
+
   const addRefrigerator = () => {
     axios.post('http://localhost:4000/refrigerator/ingredient') // 냉장고id와 식재료id를 알려줘야함
 
   }
+
+  const onChangeName = (event) => {
+    setNameValue(event.target.value)
+  }
+
+  const onChangeMethod = (event) => {
+    setMethodValue(event.target.value)
+  }
+
 
   function makeEl(el) {
     if(el[1] === undefined){
@@ -53,7 +72,12 @@ export default function Ingredients() {
     }
   }
 
-  const [exContent, setExContent] = useState('')
+  //삭제하는 버튼
+
+  const mekeDeleteBtn = () => {
+    
+  }
+
   
   
   function MouseOver(event) {
@@ -74,8 +98,12 @@ export default function Ingredients() {
       </section>
       <section className='Storage__method'>
         <h3 className='method__description'>식재료 보관방법</h3>
-        <div className='method__explain'>{exContent}</div>
 
+        {true ? <span><input onKeyUp={onChangeName} type='text' placeholder='재료이름을 입력해주세요'></input>
+        <input onKeyUp={onChangeMethod} type='text' placeholder='보관법을 작성해주세요'>
+          </input><button onClick={addIngredients}>추가</button></span> : null}
+        
+        <div className='method__explain'>{exContent}</div>
       </section>
     </div>
   )
