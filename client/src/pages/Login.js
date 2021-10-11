c045cab535f52aafa7c0128ba19d7ae5cb03d389
zzero0622 <zzero9158@gmail.com>
2021-10-11 14:09:40 +0900

    add userInput css file, userInput/Login/Mypage/SignUp component

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import axios from 'axios';
+import UserInput from '../components/UserInput';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
-  return <div>Login</div>;
+  const [userInfo, setInputInfo] = useState({
+    email: { data: '', validity: false },
+    password: { data: '', validity: false },
+  });
+
+  const handleInputValue = (key) => (e, v) => {
+    setInputInfo({ ...userInfo, [key]: { data: e.target.value, validity: v } });
+  };
+
+  const checkErr = () => {
+    for (const key in userInfo) {
+      if (!userInfo[key][`validity`]) return true;
+    }
+    return false;
+  };
+
+  const handleLogin = () => {
+    if (checkErr()) {
+      console.log('failed to submit');
+    } else {
+      // axios
+      //   .post(
+      //     'https://localhost:4000/user/signin',
+      //     {
+      //       name: userInfo.name.validity,
+      //       email: userInfo.email.validity,
+      //       password: userInfo.password.validity,
+      //     },
+      //     {
+      //       withCredentials: true, //쿠키 허용
+      //     }
+      //   )
+      //   .then((result) => {
+      //     console.log(result.config.data);
+      //   });
+    }
+  };
+
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <form id='signinForm' method='post' action='/user/signin'>
+        <UserInput item='email' type='email' handler={handleInputValue} inputInfo={userInfo} />
+        <UserInput item='password' type='password' handler={handleInputValue} inputInfo={userInfo} />
+      </form>
+      <Link to='/signup'>
+        <button type='button'>Sign Up</button>
+      </Link>
+      <button type={checkErr() ? 'button' : 'submit'} onClick={handleLogin}>
+        Sign In
+      </button>
+    </div>
+  );
 }
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,5 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import UserInput from '../components/UserInput';
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [userInfo, setInputInfo] = useState({
+    email: { data: '', validity: false },
+    password: { data: '', validity: false },
+    'password confirm': { data: '', validity: false },
+    name: { data: '', validity: false },
+  });
+
+  const handleInputValue = (key) => (e, v) => {
+    setInputInfo({ ...userInfo, [key]: { data: e.target.value, validity: v } });
+  };
+
+  const checkErr = () => {
+    for (const key in userInfo) {
+      if (!userInfo[key][`validity`]) return true;
+    }
+    return false;
+  };
+
+  const handleLogin = () => {
+    if (checkErr()) {
+      console.log('failed to submit');
+    } else {
+      // axios
+      //   .post(
+      //     'https://localhost:4000/user/signup',
+      //     {
+      //       name: userInfo.name.validity,
+      //       email: userInfo.email.validity,
+      //       password: userInfo.password.validity,
+      //     },
+      //     {
+      //       withCredentials: true, //쿠키 허용
+      //     }
+      //   )
+      //   .then((result) => {
+      //     console.log(result.config.data);
+      //   });
+    }
+  };
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <form id='signupForm' method='post' action='https://localhost:4000/user/signup'>
+        <UserInput item='email' type='email' handler={handleInputValue} inputInfo={userInfo} />
+        <UserInput item='password' type='password' handler={handleInputValue} inputInfo={userInfo} />
+        <UserInput item='password confirm' type='password' handler={handleInputValue} inputInfo={userInfo} />
+        <UserInput item='name' type='text' handler={handleInputValue} inputInfo={userInfo} />
+      </form>
+      <button type={checkErr() ? 'button' : 'submit'} onClick={handleLogin} form='signupForm'>
+        Sign Up
+      </button>
+    </div>
+  );
 }
