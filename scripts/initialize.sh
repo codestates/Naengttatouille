dfa9d15e61105dc8aafc401dd4f8d5f491b0850b
SEB_Full_30_양지영 <82949221+JIYEONGYANGdev@users.noreply.github.com>
2021-10-12 15:45:13 +0900

    initialize.sh 수정
    node 버전 명시

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -3,11 +3,12 @@
 sudo apt-get update
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
 
 nvm --version
 nvm install node 
+nvm install 14.18.0
+nvm use 14.18.0
 sudo apt install npm
 node --version
 
