bd22bf5bea71d661c5dd26d94a58bbb42f6ee6c5
SEB_Full_30_양지영 <82949221+JIYEONGYANGdev@users.noreply.github.com>
2021-10-12 15:15:09 +0900

    initialize.sh 수정(NVM)
    노드 버전 수정

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 cd /home/ubuntu/Naengttatouille/server
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+
+nvm --version
+nvm install node
+sudo apt install npm
+
 npm install
 npm install pm2@latest -g
 sudo apt-get update
