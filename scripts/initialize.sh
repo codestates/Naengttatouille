5e3f55e2d6707f8a2bb7e4177f4391af25f04d7a
SEB_Full_30_양지영 <82949221+JIYEONGYANGdev@users.noreply.github.com>
2021-10-12 14:49:49 +0900

    initialize.sh 수정

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -7,3 +7,6 @@ sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
 sudo chown ubuntu /etc/authbind/byport/80
 sudo chmod 755 /etc/authbind/byport/80
+
+sudo npx sequelize-cli db:migrate
+sudo npx sequeilze-cli db:seed:all
