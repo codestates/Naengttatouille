0e31c997c09b63e6323261e24e966fcd0903b062
SEB_Full_30_양지영 <82949221+JIYEONGYANGdev@users.noreply.github.com>
2021-10-12 15:31:42 +0900

    initialize.sh (서버 환경구축 부터)
    서버 환경구축 부터 명령어 기입

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+sudo apt-get update
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+
+nvm --version
+nvm install node 
+sudo apt install npm
+node --version
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
+
 cd /home/ubuntu/Naengttatouille/server
 
 npm install
