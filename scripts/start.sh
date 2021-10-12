dc8c121aac1caa4b413c0ecd7df0b2f6f3bde00a
SEB_Full_30_양지영 <82949221+JIYEONGYANGdev@users.noreply.github.com>
2021-10-12 14:31:04 +0900

    start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 cd /home/ubuntu/Naengttatouille/server
 
+export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_USERNAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USERNAME --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_NAME --query Parameters[0].Value | sed 's/"//g')
+export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
 export HTTP_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names HTTP_PORT --query Parameters[0].Value | sed 's/"//g')
 export CLIENT=$(aws ssm get-parameters --region ap-northeast-2 --names CLIENT --query Parameters[0].Value | sed 's/"//g')
 
