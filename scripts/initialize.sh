f0819a71dc0c702f8b1270d9fe25dddf1c4be059
SEB_Full_30_양지영 <82949221+JIYEONGYANGdev@users.noreply.github.com>
2021-10-12 17:19:47 +0900

    initialize.sh revert
    원상복귀

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt-get update
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
-
-nvm install node 
-nvm install 14.18.0
-nvm use 14.18.0
-sudo apt install npm
-
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-
 cd /home/ubuntu/Naengttatouille/server
 
-sudo npm install
-sudo npm install pm2@latest -g
+npm install
+npm install pm2@latest -g
 sudo apt-get update
 sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
