d3f8816f2d249f1319eee8ec1b290dc30211177b
SEB_Full_30_양지영 <82949221+JIYEONGYANGdev@users.noreply.github.com>
2021-10-13 14:35:10 +0900

    Update index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,18 +1,17 @@
 'use strict';
-const AWS = require('aws-sdk');
-AWS.config();
+require('dotenv').config();
 
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(AWS[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
     config.database,
