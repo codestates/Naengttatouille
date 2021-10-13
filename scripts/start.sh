61706dbc9081b011adfe4f7072536f71961aeda6
js5787 <ljs950430@gmail.com>
2021-10-13 15:41:14 +0900

    수정14

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,5 +7,6 @@ export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --name
 export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_NAME --query Parameters[0].Value | sed 's/"//g')
 export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
-
+export NODE_ENV=$(aws ssm get-parameters --region ap-northeast-2 --names NODE_ENV --query Parameters[0].Value | sed 's/"//g')
+export HTTPS_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names HTTPS_PORT --query Parameters[0].Value | sed 's/"//g')
 authbind --deep pm2 start -f index.js
