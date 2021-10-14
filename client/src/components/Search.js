a72069bf1f2cd4b9cbea8fe4dd555b381e6d39ff
zzero0622 <zzero9158@gmail.com>
2021-10-14 14:29:43 +0900

    브랜치 바꾸기용

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,36 +1,36 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
+import './Search.css';
 
 function Search({ recipeTags, handleRecipeTags, handleTag, getYoutubeVideo, searchUrl }) {
-
   return (
     <>
-          <div className='tag__container'>
+      <div className='tag__container'>
         <div className='tag__box'>
           {recipeTags.map((tag) => {
             return (
               <span key={tag} className='tag' onClick={() => handleTag('delete', tag)}>
-                {tag + '  ✘'}
+                {/* {tag + '  ✘'} */}
+                {tag}
               </span>
             );
           })}
-          </div>
+        </div>
         <div className='tagAndSearch'>
           <button className='uncheck_tags' onClick={() => handleTag('deleteAll', '')}>
             전체 선택 취소
           </button>
-        {/* <button type='button' className='recipe_search' onClick={getYoutubeVideo}>
+          {/* <button type='button' className='recipe_search' onClick={getYoutubeVideo}>
           레시피 검색
           </button> */}
 
-        <button type='button' className='recipe_search' onClick={() => getYoutubeVideo(searchUrl)}>
-          레시피 검색
-        </button>
-        {/* <button className='more' onClick={() => getYoutubeVideo(nextPageUrl)}>더보기</button> */}
-
-      </div>
+          <button type='button' className='recipe_search' onClick={() => getYoutubeVideo(searchUrl)}>
+            레시피 검색
+          </button>
+          {/* <button className='more' onClick={() => getYoutubeVideo(nextPageUrl)}>더보기</button> */}
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Search
+export default Search;
